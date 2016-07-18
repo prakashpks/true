eed7796bc2db1a05c6b48f80b3872178872f5cc0
prakashpks <prakars878740@gmail.com>
2016-07-18 18:06:57 +0530

    finish

---
+++ README.c
@@ -0,0 +1,21 @@
+# true
+#include <stdio.h>
+
+void main()
+{
+
+    int a=0;
+    if(a==0)
+    {
+        printf("false");
+    }
+        else if(a>=1 && a<=6)
+        {
+        printf("true");
+            
+        }
+        else if(a>6)
+        {
+        printf("enter valied number");
+    }}
+
